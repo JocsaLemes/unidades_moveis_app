a3234ccad0d6ec4c5685a29f7f7a4d2f9406553b
cassiojuniorr <cassiospcjunior@gmail.com>
2025-01-06 20:23:28 -0300

    front tela de login terminada

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -16,21 +16,53 @@ class _LoginPage extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 20, 52, 109),
+      backgroundColor: const Color.fromRGBO(33, 50, 99, 1),
       body: Center(
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              //Image.asset(),
               Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                padding: const EdgeInsets.all(40),
+                child: Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Unidades \n",
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'Móveis',
+                        style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                child: Image.asset(
+                  "assets/logoUnidadeMoveis.png",
+                  height: 250,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(50, 0, 50, 40),
                 child: TextFormField(
                   controller: _nameController,
                   decoration: InputDecoration(
                       filled: true,
-                      fillColor: const Color.fromARGB(255, 99, 116, 146),
+                      fillColor: const Color.fromRGBO(108, 122, 162, 1),
                       hintText: "Nome",
                       hintStyle: const TextStyle(
                         color: Color.fromRGBO(255, 255, 255, 1),
@@ -43,13 +75,13 @@ class _LoginPage extends State<LoginPage> {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                padding: const EdgeInsets.fromLTRB(50, 0, 50, 40),
                 child: TextFormField(
                   controller: _passwordController,
                   obscureText: true,
                   decoration: InputDecoration(
                       filled: true,
-                      fillColor: const Color.fromARGB(255, 99, 116, 146),
+                      fillColor: const Color.fromRGBO(108, 122, 162, 1),
                       hintText: "Senha",
                       hintStyle: const TextStyle(
                         color: Color.fromRGBO(255, 255, 255, 1),
@@ -64,7 +96,7 @@ class _LoginPage extends State<LoginPage> {
               ElevatedButton(
                   onPressed: () {},
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromRGBO(6, 14, 53, 1),
+                    backgroundColor: const Color.fromRGBO(7, 13, 32, 1),
                     foregroundColor: Colors.white,
                     shadowColor: Colors.black,
                     shape: RoundedRectangleBorder(
